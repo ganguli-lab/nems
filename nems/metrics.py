64ec8e3adfc810abc113fb0469b761915608b69b
Niru Maheswaranathan <nirum@stanford.edu>
2016-01-31 23:50:34 -0800

    cleaned up metrics

--- nems/metrics.py
+++ nems/metrics.py
@@ -1,6 +1,6 @@
 import numpy as np
+from scipy.stats import pearsonr
 from collections import namedtuple
-
 
 Score = namedtuple('Score', ['cc', 'lli', 'rmse', 'fev'])
 scorenames = {
@@ -12,17 +12,12 @@ scorenames = {
 
 
 def cc(r, rhat):
-    """
-    Correlation coefficient
-    """
-    return np.corrcoef(np.vstack((rhat, r)))[0, 1]
+    """Pearson's correlation coefficient"""
+    return pearsonr(r, rhat)[0]
 
 
 def lli(r, rhat, dt=1e-2):
-    """
-    Log-likelihood improvement over a mean rate model (in bits per spike)
-    """
-
+    """Log-likelihood improvement over a mean rate model (in bits per spike)"""
     # mean firing rate
     mu = np.mean(r)
 
@@ -35,17 +30,13 @@ def lli(r, rhat, dt=1e-2):
 
 
 def rmse(r, rhat):
-    """
-    Root mean squared error
-    """
+    """Root mean squared error"""
     return np.sqrt(np.mean((rhat - r) ** 2))
 
 
 def fev(r, rhat):
-    """
-    Fraction of explained variance
-    """
+    """Fraction of explained variance
 
-    mean = np.mean(r)
-    rate_var = np.mean((mean - r) ** 2)
-    return 1.0 - (rmse(r, rhat) ** 2) / rate_var
+    https://wikipedia.org/en/Fraction_of_variance_unexplained
+    """
+    return 1.0 - rmse(r, rhat)**2 / r.var()
