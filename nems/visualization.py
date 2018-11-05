7d8cd9899c5643061d14f28b1dfb89bbff25d45d
szapp <soeren.zapp@med.uni-goettingen.de>
2018-11-05 12:52:22 +0100

    Allow selection of spatial axis for visualization

--- nems/visualization.py
+++ nems/visualization.py
@@ -30,7 +30,7 @@ def contour(W, n=3, **kwargs):
     plt.contour(-W, n, **kwargs)
 
 
-def plot(mdl, cmap='seismic'):
+def plot(mdl, cmap='seismic', axis=0):
     """
     Plots the parameters of a NeuralEncodingModel
 
@@ -46,6 +46,20 @@ def plot(mdl, cmap='seismic'):
     # Plot the filters.
     for j in range(nsub):
         W = mdl.theta['W'][j]
+
+        # Reshape filter if three dimensional.
+        if len(mdl.filter_dims) > 2:
+            W = W.reshape(mdl.filter_dims)
+            assert axis == 0 or axis == 1, "Invalid spatial axis"
+
+            # Get maximum intensity value from the other spatial axis.
+            W = np.rollaxis(W, 1-axis)
+            maxi = np.unravel_index(np.abs(W).argmax(),
+                                    mdl.filter_dims)[0]
+
+            # Discard the other spatial axis.
+            W = W[maxi]
+
         ax = fig.add_subplot(2, nsub, j + 1)
         ax.pcolor(W, cmap=cm.__dict__[cmap], vmin=-maxval, vmax=maxval)
         ax.set_xticks([])
